4f7faabb79b9d946f447be6dabb18ecc992a72dd
Idrees Mohammed <idreesaud@gmail.com>
2022-11-29 22:29:39 -0600

    constructor being stupid about airports

--- src/airport.h
+++ src/airport.h
@@ -1,3 +1,4 @@
+#pragma once
 #include "utils.h"
 
 struct airport {
--- src/graph.cpp
+++ src/graph.cpp
@@ -1,9 +1,26 @@
 #include "graph.h"
 
 Graph::Graph(std::string airport_csv, std::string routes_csv) {
+    // Reading into v2d
+    std::vector<std::vector<std::string>> airports = CsvTwoD(airport_csv);
+    std::vector<std::vector<std::string>> routes = CsvTwoD(routes_csv);
 
+    // populate airports
+    for (auto& ap : airports) {
+        airport curr_airport;
+        curr_airport = createAirport(ap);
+        airports.push_back(curr_airport);
+    }
 }
 
 int Graph::getDistance(std::string airport_one, std::string airport_two) {
-    
+    (void) airport_one;
+    (void) airport_two;
+    return 0;
+}
+
+airport Graph::createAirport(std::vector<std::string> line) {
+    (void) line;
+    airport ap;
+    return ap;
 }
