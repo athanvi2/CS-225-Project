b38828ba30b9c2d2631815c0ac4a1a54f5a63d51
kasyeda2 <kasyeda2@illinois.edu>
2022-11-30 09:14:11 -0600

    tests!

---
+++ tests/test.cpp
@@ -0,0 +1,23 @@
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "../graph.h"
+#include "../utils.h"
+#include "../airport.h"
+
+
+TEST_CASE("Invalid Airport Code (Calc Dist)") {
+    std::string airport_one = '';
+    std::string airport_two = 'zzz';
+    double result = calcEdgeDistance(airport_one, airport_two);
+    REQUIRE(result == -1);
+}
+
+TEST_CASE("Valid Airport Code (Calc Dist)") {
+    std::string airport_one = 'GKA';
+    std::string airport_two = 'MAG';
+    double result = calcEdgeDistance(airport_one, airport_two);
+    REQUIRE(int (result) == 107);
+}
